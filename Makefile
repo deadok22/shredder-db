679f302e0c8b0a9f413d43ecbd47fdc5b6f5141a
HatlessFox <hatless.fox@gmail.com>
2012-12-16 03:08:31 +0400

    Minor bug fixes, fix code style

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
-D_KEY = -DDEBUG 
+D_KEY = -DDEBUG
 COPTS = -std=c++0x -D_GLIBCXX_FULLY_DYNAMIC_STRING -Wall
 
 all: build
--- src/backend/BufferManager.cpp
+++ src/backend/BufferManager.cpp
@@ -5,14 +5,11 @@ BufferManager & BufferManager::get_instance() {
   return bm;
 }
 
-BufferManager::BufferManager()
- : max_size_(InfoPool::get_instance()->get_db_info()->max_page_cnt)
-{  
+BufferManager::BufferManager(): max_size_(InfoPool::get_instance()->get_db_info()->max_page_cnt) {  
   Utils::log("[BufferManager] create object buffmanager");
 }
 
-BufferManager::~BufferManager()
-{
+BufferManager::~BufferManager() {
   Utils::log("[BufferManager] call destructor in buffer, save and free page");        
   for(vector<Page*>::iterator i = buffer_.begin(),
   e = buffer_.end(); i != e;++i){
@@ -21,8 +18,7 @@ BufferManager::~BufferManager()
   }
 }
 
-void BufferManager::purge()
-{
+void BufferManager::purge() {
   Utils::log("[BufferManager] purge buffer");        
   for(vector<Page*>::iterator i = buffer_.begin(),
   e = buffer_.end(); i != e;++i){
@@ -32,23 +28,21 @@ void BufferManager::purge()
   buffer_.clear();
 }
 
-void BufferManager::force()
-{
+void BufferManager::force() {
   Utils::log("[BufferManager] force pages");        
-  for(vector<Page*>::iterator i = buffer_.begin(),
-  e = buffer_.end(); i != e;++i)
+  for(vector<Page*>::iterator i = buffer_.begin(), e = buffer_.end(); i != e; ++i) {
     disk_mng_.write_page(*i);
+  }
 }
 
-Page& BufferManager::get_page(size_t page_id,string const& fname)
-{
+Page& BufferManager::get_page(size_t page_id,string const& fname) {
   Utils::log("[BufferManager] get page with page id: "+std::to_string(page_id) );        
   Page* p = 0;
   if( !( p = find_page(page_id,fname)) ){
-    p = new Page(page_id,fname); // pin default    
+    p = new Page(page_id, fname); // pin default    
     if( buffer_.size() < max_size_){
       // append in back
-      if( disk_mng_.read_page(p) ){
+      if(disk_mng_.read_page(p) ){
         buffer_.push_back(p);
         Utils::log("[BufferManager] page appended in buffer, current size buffer: "+ std::to_string(buffer_.size()) );
       } else {
@@ -66,9 +60,7 @@ Page& BufferManager::get_page(size_t page_id,string const& fname)
   return *p;   
 }
 
-
-Page* BufferManager::find_page(size_t page_id,string const& fname)
-{
+Page* BufferManager::find_page(size_t page_id,string const& fname) {
   for(vector<Page*>::iterator i = buffer_.begin(),
   e = buffer_.end(); i != e;++i)
     if( (*i)->get_pid() == page_id && (*i)->get_fname() == fname ){
@@ -79,41 +71,27 @@ Page* BufferManager::find_page(size_t page_id,string const& fname)
   return 0;  
 }
 
-
-
-
-vector<Page*>::iterator BufferManager::find_unpinned_page()
-{
-  for(vector<Page*>::iterator i = buffer_.begin(),
-  e = buffer_.end(); i != e;++i)
-    if( (*i)->is_unpinned()){
-      Utils::log("[BufferManager] found unpinned page in buffer: "+ std::to_string( (*i)->get_pid() ) ) ;      
+vector<Page*>::iterator BufferManager::find_unpinned_page() {
+  for(vector<Page*>::iterator i = buffer_.begin(), e = buffer_.end(); i != e; ++i) {
+    if((*i)->is_unpinned()){
+      Utils::log("[BufferManager] found unpinned page in buffer: " + std::to_string((*i)->get_pid()) );      
       return i;
     }
+  }
   Utils::log("[BufferManager] not found unpinned page in buffer");
   return buffer_.end();  
 }
 
-
-bool BufferManager::replace(Page * p)
-{
+bool BufferManager::replace(Page * p) {
   vector<Page*>::iterator i;
   if( (i = find_unpinned_page()) != buffer_.end()){
     Utils::log("[BufferManager] replace page and delete unpinned page");    
     if( disk_mng_.write_page(*i) ){
       delete *i;
       *i = p;
-      if( disk_mng_.read_page(p) )
-        return true;
+      if( disk_mng_.read_page(p)) { return true; }
     }
   }
   Utils::log("[BufferManager] method replace is failed", ERROR);
   return false;
 }
-
-
-
-
-
-
-
