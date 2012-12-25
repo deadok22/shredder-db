e536b59f3f1408eb8d1f6e13fa67cafee2680802
vlomshakov <vadim.lomshakov@gmail.com>
2012-12-26 03:17:07 +0400

    implement CsvPrinter

---
+++ src/core/CsvPrinter.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "TableMetadata.pb.h"
+#include "MetaDataProvider.h"
+#include "RecordsIterator.h"
+#include "../common/DataType.h"
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using std::stringstream;
+using std::string;
+
+class CsvPrinter {
+public:
+  static CsvPrinter & get_instance();
+  string get_csv( RecordsIterator const& rec_iter, string const & table_name );
+  string get_header_csv( string const & table_name );
+
+private:
+  const char DELIM; 
+  CsvPrinter();
+  ~CsvPrinter();
+};
+
+
+
+
+
+#ifdef TEST_CSV_P
+
+int main()
+{
+
+
+  return 0;
+}
+
+#endif
